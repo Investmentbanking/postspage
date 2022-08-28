9924c373214023fe9ee3d72bd2d2c1554e998244
Investmentbanking <41571606+Investmentbanking@users.noreply.github.com>
2022-08-28 21:17:22 +0100

    Initialization

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("Express is working");
+});
+
+
+app.listen(3001, () => {
+    console.log("app listening on port 3001");
+});
